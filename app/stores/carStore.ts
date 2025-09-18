c8f4b191b2e8dbf4bf8f8158e5bba667b8467dc4
Arthur Kim <artur.kim@south.rt.ru>
2025-09-18 22:35:26 +0900

    add pinia persisted plugin

--- app/stores/carStore.ts
+++ app/stores/carStore.ts
@@ -2,23 +2,28 @@ import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 import type { CarInfo } from "../types/car"
 
-export const useCarStore = defineStore('car', () => {
-  const carInfo = ref<CarInfo|null>(null)
-  const error = ref(false)
+export const useCarStore = defineStore('car',
+  () => {
+    const carInfo = ref<CarInfo|null>(null)
+    const error = ref(false)
 
-  const carName = computed(() => carInfo.value ? `${carInfo.value.make} ${carInfo.value.model}` : '')
+    const carName = computed(() => carInfo.value ? `${carInfo.value.make} ${carInfo.value.model}` : '')
 
-  const carInfoOrder = ['make', 'model', 'submodel', 'year', 'description']
+    const carInfoOrder = ['make', 'model', 'submodel', 'year', 'description']
 
-  function setCarInfo(data: CarInfo) {
-    carInfo.value = data
+    function setCarInfo(data: CarInfo) {
+      carInfo.value = data
+    }
+
+    return {
+      carInfo,
+      carInfoOrder,
+      carName,
+      setCarInfo,
+      error
+    }
+  },
+  {
+    persist: true,
   }
-
-  return {
-    carInfo,
-    carInfoOrder,
-    carName,
-    setCarInfo,
-    error
-  }
-})
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image', '@pinia/nuxt'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/image',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt'
+  ],
   css: ['~/assets/css/main.css'],
   ui: {
     colorMode: false,
